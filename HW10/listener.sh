d48aaebb6db5451609d79efa7d229e1f843cf0f5
Wylliam <chengwaiyuen27112003@gmail.com>
2023-12-08 18:33:19 -0500

    HW10 completed

--- HW10/listener.sh
+++ HW10/listener.sh
@@ -1,9 +1,6 @@
 #! /bin/bash
-
 echo "setting up"
-
 sudo apt-get update
 sudo apt-get -y install python3 python3-pip
 python3 -m pip install google-cloud-pubsub
-
 gsutil cp gs://ds561-hw10-wyc/listener.py /listener.py
